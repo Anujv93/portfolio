2bf26595506ab343803ea66c6ca3b83fd93eb68f
Anujv93 <anujv1212@gmail.com>
2023-01-21 12:51:41 +0530

    Update with Animation

--- portfolio/index.js
+++ portfolio/index.js
@@ -4,14 +4,14 @@ ScrollTrigger.create({
     trigger: ".work_exp",
     pin: '.work_heading',
     start: "top 150px",
-    end: "bottom 350px",
-    markers: true
+    end: "bottom 300px",
+    markers: false
 })
 
 ScrollTrigger.create({
     trigger:'.projects',
     pin:'.project_heading',
     start:'top 150px',
-    end: 'bottom 350px',
-    markers: true
+    end: 'bottom 300px',
+    markers: false
 })
