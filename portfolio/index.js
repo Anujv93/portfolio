05b3e60e263ce952cecddcc93e4d001e21bde893
Tanay0708 <123vyast@gmail.com>
2023-01-23 00:01:20 +0530

    added

--- portfolio/index.js
+++ portfolio/index.js
@@ -15,3 +15,11 @@ ScrollTrigger.create({
     end: 'bottom 300px',
     markers: false
 })
+
+ScrollReveal().reveal('.courses',{delay:200})
+ScrollReveal().reveal('.about_me',{delay: 200});
+ScrollReveal().reveal('.work_heading');
+ScrollReveal().reveal('.work_list')
+ScrollReveal().reveal('project_heading',{delay: 200})
+ScrollReveal().reveal('.projects_list',{delay: 200});
+ScrollReveal().reveal('.get_in_touch',{delay: 200})
